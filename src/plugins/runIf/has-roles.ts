e169b7672cc0d0c1b65912250170082c327a0154
Joe Ferner <joe@fernsroth.com>
2017-07-07 20:35:47 -0400

    support or expressions in has roles

--- src/plugins/runIf/has-roles.ts
+++ src/plugins/runIf/has-roles.ts
@@ -2,7 +2,13 @@ import {ExecutionContext, RunIfPlugin, Script} from "../../../types";
 
 export class HasRolesRunIfPlugin implements RunIfPlugin {
     async shouldExecute(ctx: ExecutionContext, script: Script, args: string[]): Promise<boolean> {
-        for (let role of args) {
+        const expressions = args.join(' ').split('||');
+        return expressions.some(expr => HasRolesRunIfPlugin.evalAndExpression(ctx, expr));
+    }
+
+    private static evalAndExpression(ctx: ExecutionContext, expr: string): boolean {
+        const roles = expr.trim().replace(/\s+/g, ' ').split(' ');
+        for (let role of roles) {
             if (ctx.roles.indexOf(role) < 0) {
                 return false;
             }
